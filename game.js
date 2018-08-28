affb2826260e42a31d19cd7caaea2ec80c07ddb5
Rostislav Novák <rostislav.novak92@gmail.com>
2018-08-28 21:20:24 +0200

    Implemented showing player.

--- game.js
+++ game.js
@@ -6,10 +6,10 @@ const canvasBg = document.getElementById("canvasBg"),
 const canvasWidth = canvasBg.width,
     canvasHeight = canvasBg.height;
 
-    // player1 = new Player();
-    // enemies = [],
-    // numEnemies = 5,
-    // obstacles = []
+ player = new Player();
+// enemies = [],
+// numEnemies = 5,
+// obstacles = []
 let isPlaying = false;
 
 // || Means that if not exist it will try next and last line safe line.
@@ -41,7 +41,77 @@ function begin() {
     requestAnimationFrame(loop)
 }
 
+function update() {
+    clearCtx(ctxEntities);
+    // updateAllEnemies();
+    player.update();
+}
+
+function draw() {
+    // drawAllEnemies
+    player.draw();
+}
+
 // main game loop
 function loop() {
-
+    if (isPlaying) {
+        update();
+        draw();
+        requestAnimationFrame(loop);
+    }
 }
+
+function clearCtx(ctx) {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
+// Defining object player
+function Player() {
+    // Image of player in sprites image
+    this.srcX = 0;
+    this.srcY = 480;
+    this.width = 16;
+    this.height = 32;
+    // Where player should be draw
+    this.drawX = 224;
+    this.drawY = 336;
+    // Center of player for collision
+    this.centerX = this.drawX + (this.width / 2);
+    this.centerY = this.drawY + (this.height / 2);
+    // Speed of moving player.
+    this.speed = 2;
+    // Control keys
+    // this.isUpKey = false;
+    // this.isRightKey = false;
+    // this.isDownKey = false;
+    // this.isLeftKey = false;
+    // this.isSpacebar = false;
+    // this.isShooting = false;
+    // Shooting variables
+    //var numBullets = 10;
+    // this.bullets = [];
+    // this.currentBullet = 0;
+    // for (var i = 0; i < numBullets; i++) {
+    //     this.bullets[this.bullets.length] = new Bullet();
+    // }
+}
+
+// Adding method to object
+Player.prototype.update = function () {
+    this.centerX = this.drawX + (this.width / 2);
+    this.centerY = this.drawY + (this.height / 2);
+
+    //this.checkDirection();
+    //this.checkShooting();
+    //this.updateAllBullets();
+};
+
+Player.prototype.draw = function () {
+    // this.drawAllBullets();
+    ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+};
+
